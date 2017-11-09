a7ff8814c79757832090b37147cee39d2826d413
Anthony Bonante <abonante@premierautomation.com>
2017-11-09 16:38:47 -0500

    Initial testing

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+        html {
+            background: url(_img/test.jpg);
+            overflow: hidden;
+        }
+
+    </style>
+</head>
+
+<body>
+    <?php include('snow.php'); ?>
+</body>
+
+</html>
